e0bdbad096353f26304f54fd257c590292ad44ac
Gülbahar Erol <ggulbaharerol@gmail.com>
2022-09-28 08:36:27 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+
+const App= () => {
+
+  return (
+    <SafeAreaView>
+    </SafeAreaView>
+  );
+};
+
+export default App;
